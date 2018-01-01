df553098f7618ab9404eec791bec51c644d31be1
surendirababu <babusuren@hotmail.com>
2018-01-01 19:31:46 +0530

    altered script

--- src/main/webapp/sql_scripts/script_002_postgres.sql
+++ src/main/webapp/sql_scripts/script_002_postgres.sql
@@ -6,3 +6,26 @@ CREATE TABLE EMPLOYEE(
       salary double precision NOT NULL,
       ssn VARCHAR(30) NOT NULL UNIQUE
   );
+
+
+ CREATE TABLE userdetail(
+      id int NOT NULL PRIMARY KEY,
+      user_name VARCHAR(50) NOT NULL,
+      password VARCHAR(50) NOT NULL,
+      email VARCHAR(50) NOT NULL
+  );
+
+  CREATE TABLE roledetail(
+      id int NOT NULL PRIMARY KEY,
+      role_name VARCHAR(50) NOT NULL,
+       user_id int not null
+  );
+
+  insert into userdetail (id,user_name,password,email) values
+  (1,'babu','babu636009','babu@hotmail.com')
+
+   insert into userdetail (id,user_name,password,email) values
+  (2,'suren','suren36009','suren@hotmail.com')
+
+  insert into roledetail (id,role_name,user_id) values
+    (1,'admin',1)
